b4e0b9bdd49fdbff59770fb267b689dc973b6ef4
lollol0613 <mhrainaji@gmail.com>
2019-05-12 16:15:39 +1200

    UI draft

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>KIWI TODO</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"}/> 
+        </View>
       </View>
     );
   }
@@ -14,8 +20,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e85359',
+    alignItems: 'center'
   },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: "400",
+    marginTop: 50,
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width-25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      andorid: {
+        elevation: 3
+      }
+    })
+  }
 });
